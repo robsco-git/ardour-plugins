793fdc2ad391b06560b4d365505b182134d20176
Robert Scott <rs@robsco.info>
2019-11-22 12:03:36 +0200

    Use spaces instead of tabs

--- bitcrush.lua
+++ bitcrush.lua
@@ -1,18 +1,18 @@
 ardour {
-	["type"]    = "dsp",
-	name        = "a-Bitcrush",
-	category    = "Bitcrush",
-	license     = "MIT",
-	author      = "Robert Scott",
-	description = [[Apply bit reduction and/or downsampling to the audio signal.]]
+    ["type"]    = "dsp",
+    name        = "a-Bitcrush",
+    category    = "Bitcrush",
+    license     = "MIT",
+    author      = "Robert Scott",
+    description = [[Apply bit reduction and/or downsampling to the audio signal.]]
 }
 
 function dsp_ioconfig ()
-	return
-	{
-		-- -1, -1 = any number of channels as long as input and output count matches
-		{ audio_in = -1, audio_out = -1},
-	}
+    return
+    {
+        -- -1, -1 = any number of channels as long as input and output count matches
+        { audio_in = -1, audio_out = -1},
+    }
 end
 
 local sample_rate
@@ -22,16 +22,16 @@ function dsp_init (rate)
 end
 
 function dsp_params ()
-	return {
+    return {
         { ["type"] = "input", name = "Bit depth", min = 1, max = 32, default = 8, unit="bit depth"},
         { ["type"] = "input", name = "Sample rate", min = 1, max = sample_rate, default = 11025, unit="sample rate"},
     }
 end
 
 function dsp_configure (ins, outs)
-	local audio_ins = ins:n_audio();
-	local audio_outs = outs:n_audio()
-	assert (audio_ins == audio_outs)
+    local audio_ins = ins:n_audio();
+    local audio_outs = outs:n_audio()
+    assert (audio_ins == audio_outs)
 end
 
 local function round (f)
@@ -64,21 +64,22 @@ function dsp_run (ins, outs, n_samples)
 
     prev_rate = current_rate
     local max = (2 ^ bit_depth) - 1;
-    local step = math.floor(sample_rate / current_rate); -- The number of samples that need to have the same value based on the session's sample rate    
+    local step = math.floor(sample_rate / current_rate); -- The number of samples that need to have the same value based on the session's sample rate
 
-	for c = 1, #outs do -- for each output channel (count from 1 to number of output channels)
-		if ins[c] ~= outs[c] then -- if processing is not in-place..
-			ARDOUR.DSP.copy_vector (outs[c], ins[c], n_samples) -- ..copy data from input to output.
+    for c = 1, #outs do -- for each output channel (count from 1 to number of output channels)
+
+        if ins[c] ~= outs[c] then -- if processing is not in-place..
+            ARDOUR.DSP.copy_vector (outs[c], ins[c], n_samples) -- ..copy data from input to output.
         end
 
         if not sample_sums[c] then
             sample_sums[c] = 1
         end
+        
+        -- direct audio data access, in-place processing of output buffer
+        local buf = outs[c]:array() -- get channel's 'c' data as lua array reference
 
-		-- direct audio data access, in-place processing of output buffer
-		local buf = outs[c]:array() -- get channel's 'c' data as lua array reference
-
-		-- process all audio samples
+        -- process all audio samples
         for s = 1, n_samples do
             if step == 1 then
                 buf[s] = change_bitdepth(buf[s], max);
@@ -94,6 +95,6 @@ function dsp_run (ins, outs, n_samples)
                 sample_sums[c] = sample_sums[c] + 1
                 buf[s] = key_samples[c]
             end
-		end
-	end
+        end
+    end
 end
--- distort.lua
+++ distort.lua
@@ -1,127 +1,127 @@
 ardour {
-	["type"]    = "dsp",
-	name        = "a-Distort",
-	category    = "Distort",
-	license     = "GPLv2",
-	author      = "Robert Scott",
-	description = [[A distortion plugin with a number of algorithms to choose from.]]
+    ["type"]    = "dsp",
+    name        = "a-Distort",
+    category    = "Distort",
+    license     = "GPLv2",
+    author      = "Robert Scott",
+    description = [[A distortion plugin with a number of algorithms to choose from.]]
 }
 
 function dsp_ioconfig()
-	return
-	{
-		-- -1, -1 = any number of channels as long as input and output count matches
-		{ audio_in = -1, audio_out = -1},
-	}
+    return
+    {
+        -- -1, -1 = any number of channels as long as input and output count matches
+        { audio_in = -1, audio_out = -1},
+    }
 end
 
 function dsp_params()
-	return
-	{
-		{ ["type"] = "input", name = "Type", min = 0, max = 1, default = 1, enum = true, scalepoints =
-			{
-				["LoFi"] = 0,
-				["DAFX"] = 1,
-				["Hard clip"] = 2,
-				["Soft clip"] = 3,
-				["Smash"] = 4,
-			}
-		},
-		{ ["type"] = "input", name = "Input Gain", min = -20, max = 60, default = 0, unit="dB"},
-		{ ["type"] = "input", name = "Output Gain", min = -40, max = 20, default = 0, unit="dB"},
-	}
+    return
+    {
+        { ["type"] = "input", name = "Type", min = 0, max = 1, default = 1, enum = true, scalepoints =
+            {
+                ["LoFi"] = 0,
+                ["DAFX"] = 1,
+                ["Hard clip"] = 2,
+                ["Soft clip"] = 3,
+                ["Smash"] = 4,
+            }
+        },
+        { ["type"] = "input", name = "Input Gain", min = -20, max = 60, default = 0, unit="dB"},
+        { ["type"] = "input", name = "Output Gain", min = -40, max = 20, default = 0, unit="dB"},
+    }
 end
 
 function dsp_configure(ins, outs)
-	audio_ins = ins:n_audio();
-	local audio_outs = outs:n_audio()
-	assert (audio_ins == audio_outs)
+    audio_ins = ins:n_audio();
+    local audio_outs = outs:n_audio()
+    assert (audio_ins == audio_outs)
 end
 
 local function lofi_distort(f)
-	-- Based on LoFi as a part of the CMT collection
-	-- https://www.ladspa.org/cmt/overview.html
-	-- https://searchcode.com/file/18573523/cmt/src/lofi.cpp
-	if f > 0 then
-		return (f * 1) / (f + 1) * 2
-	else
-		return -1* (-1 * f * 1) / (-1 * f + 1) * 2
-	end
+    -- Based on LoFi as a part of the CMT collection
+    -- https://www.ladspa.org/cmt/overview.html
+    -- https://searchcode.com/file/18573523/cmt/src/lofi.cpp
+    if f > 0 then
+        return (f * 1) / (f + 1) * 2
+    else
+        return -1* (-1 * f * 1) / (-1 * f + 1) * 2
+    end
 end
 
 local function dafx_distort(f)
-	-- Based on formula in DAFX book by Udo Zölzer
-	-- https://dsp.stackexchange.com/a/28962
-	if (f > 0) then
-   		return 1 - math.exp(-f);
-	else
-		return -1 + math.exp(f);
-	end
+    -- Based on formula in DAFX book by Udo Zölzer
+    -- https://dsp.stackexchange.com/a/28962
+    if (f > 0) then
+           return 1 - math.exp(-f);
+    else
+        return -1 + math.exp(f);
+    end
 end
 
 local function hard_clip(f)
-	-- https://www.dsprelated.com/freebooks/pasp/Nonlinear_Distortion.html
-	if f > 1 then
-		return 1
-	elseif f < -1 then
-		return -1
-	else
-		return f
-	end
+    -- https://www.dsprelated.com/freebooks/pasp/Nonlinear_Distortion.html
+    if f > 1 then
+        return 1
+    elseif f < -1 then
+        return -1
+    else
+        return f
+    end
 end
 
 local function soft_clip(f)
-	-- https://www.dsprelated.com/freebooks/pasp/Nonlinear_Distortion.html
-	if f >= 1 then
-		return 2 / 3
-	elseif f <= -1 then
-		return -2 / 3
-	else
-		return f - (f ^ 3) / 3
-	end
+    -- https://www.dsprelated.com/freebooks/pasp/Nonlinear_Distortion.html
+    if f >= 1 then
+        return 2 / 3
+    elseif f <= -1 then
+        return -2 / 3
+    else
+        return f - (f ^ 3) / 3
+    end
 end
 
 local function smash(f)
-	-- https://github.com/Ardour/ardour/blob/master/scripts/_smash.lua
-	return math.atan(1.5707 * f) -- some non-linear gain.
+    -- https://github.com/Ardour/ardour/blob/master/scripts/_smash.lua
+    return math.atan(1.5707 * f) -- some non-linear gain.
 end
 
 function dsp_runmap(bufs, in_map, out_map, n_samples, offset)
-	local ctrl = CtrlPorts:array() -- get control port array (read/write)
+    local ctrl = CtrlPorts:array() -- get control port array (read/write)
 
-	local dist_type = ctrl[1]
-	local dist_func
-	if dist_type == 0 then
-		dist_func = lofi_distort
-	elseif dist_type == 1 then
-		dist_func = dafx_distort
-	elseif dist_type == 2 then
-		dist_func = hard_clip
-	elseif dist_type == 3 then
-		dist_func = soft_clip
-	else
-		dist_func = smash
-	end
+    local dist_type = ctrl[1]
+    local dist_func
+    if dist_type == 0 then
+        dist_func = lofi_distort
+    elseif dist_type == 1 then
+        dist_func = dafx_distort
+    elseif dist_type == 2 then
+        dist_func = hard_clip
+    elseif dist_type == 3 then
+        dist_func = soft_clip
+    else
+        dist_func = smash
+    end
 
-	local input_gain = ARDOUR.DSP.dB_to_coefficient(ctrl[2])
-	local output_gain = ARDOUR.DSP.dB_to_coefficient(ctrl[3])
+    local input_gain = ARDOUR.DSP.dB_to_coefficient(ctrl[2])
+    local output_gain = ARDOUR.DSP.dB_to_coefficient(ctrl[3])
 
-	for c = 1,audio_ins do
-		local ib = in_map:get(ARDOUR.DataType("audio"), c - 1);
-		local ob = out_map:get(ARDOUR.DataType("audio"), c - 1);
+    for c = 1,audio_ins do
+        local ib = in_map:get(ARDOUR.DataType("audio"), c - 1);
+        local ob = out_map:get(ARDOUR.DataType("audio"), c - 1);
 
-		if ib == ARDOUR.ChanMapping.Invalid and ob ~= ARDOUR.ChanMapping.Invalid then
-			bufs:get_audio(ob):silence(n_samples, offset)
-			goto nextchannel
-		end
+        if ib == ARDOUR.ChanMapping.Invalid and ob ~= ARDOUR.ChanMapping.Invalid then
+            bufs:get_audio(ob):silence(n_samples, offset)
+            goto nextchannel
+        end
 
-		local i = bufs:get_audio(ib):data(offset):array()
-		local o = bufs:get_audio(ob):data(offset):array()
+        local i = bufs:get_audio(ib):data(offset):array()
+        local o = bufs:get_audio(ob):data(offset):array()
 
-		for s = 1,n_samples do
-			o[s] = dist_func(i[s] * input_gain) * output_gain
-		end
+        for s = 1,n_samples do
+            o[s] = dist_func(i[s] * input_gain) * output_gain
+        end
 
-		::nextchannel::
-	end
+        ::nextchannel::
+    end
 end
