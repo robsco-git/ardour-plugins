761cf7917f9052c5102f6e222ee4cb084c441491
Robert Scott <rs@robsco.info>
2019-11-25 19:45:11 +0200

    Progress on valve saturation

--- valve.lua
+++ valve.lua
@@ -1,5 +1,36 @@
 -- Based on https://github.com/swh/lv2/blob/master/plugins/valve-swh.lv2/plugin.xml
 
+ardour {
+    ["type"]    = "dsp",
+    name        = "Valve",
+    category    = "Saturation",
+    license     = "GPLv2",
+    author      = "Robert Scott",
+    description = [[A valve saturation plugin.]]
+}
+
+function dsp_ioconfig()
+    return
+    {
+        -- -1, -1 = any number of channels as long as input and output count matches
+        { audio_in = -1, audio_out = -1},
+    }
+end
+
+function dsp_params()
+    return
+    {
+        { ["type"] = "input", name = "Distortion", min = 0, max = 1, default = 0.5, unit="dB"},
+        { ["type"] = "input", name = "Hardness", min = 0, max = 1, default = 0.5, unit="dB"},
+    }
+end
+
+function dsp_configure(ins, outs)
+    audio_ins = ins:n_audio();
+    local audio_outs = outs:n_audio()
+    assert (audio_ins == audio_outs)
+end
+
 -- <?xml version="1.0"?>
 -- <!DOCTYPE ladspa SYSTEM "ladspa-swh.dtd">
 -- <?xml-stylesheet href="ladspa.css" type="text/css"?>
@@ -29,8 +60,74 @@ function round_to_zero(f)
     return f
 end
 
+-- y = e^x
+function f_exp(x)
+    return math.exp(x)
+end
+
 local itm1 = 0.0
 local otm1 = 0.0
+
+function dsp_runmap(bufs, in_map, out_map, n_samples, offset)
+    local ctrl = CtrlPorts:array() -- get control port array (read/write)
+    -- q_p is a control between 0 and 1 - distortion level
+    local q_p = ctrl[1]
+    local q = q_p - 0.999
+
+    -- dist_p is a control between 0 and 1 - hardness level
+    local dist_p = ctrl[2]
+    local dist = dist_p * 40.0 + 0.1
+
+    -- local input_gain = ARDOUR.DSP.dB_to_coefficient(ctrl[2])
+    -- local output_gain = ARDOUR.DSP.dB_to_coefficient(ctrl[3])
+
+    for c = 1,audio_ins do
+        local ib = in_map:get(ARDOUR.DataType("audio"), c - 1);
+        local ob = out_map:get(ARDOUR.DataType("audio"), c - 1);
+
+        if ib == ARDOUR.ChanMapping.Invalid and ob ~= ARDOUR.ChanMapping.Invalid then
+            bufs:get_audio(ob):silence(n_samples, offset)
+            goto nextchannel
+        end
+
+        local i = bufs:get_audio(ib):data(offset):array()
+        local o = bufs:get_audio(ob):data(offset):array()
+
+        -- for s = 1,n_samples do
+            -- o[s] = dist_func(i[s] * input_gain) * output_gain
+
+        if q == 0 then
+            for pos = 1,n_samples
+            do
+                if i[pos] == q then
+                    fx = 1.0 / dist
+                else
+                    fx = i[pos] / (1.0 - f_exp(-dist * i[pos]))
+                end
+                otm1 = 0.999 * otm1 + fx - itm1
+                round_to_zero(otm1)
+                itm1 = fx
+                o[pos] = otm1
+            end
+        else
+            for pos = 1,n_samples
+            do
+                if i[pos] == q then
+                    fx = 1.0 / dist + q / (1.0 - f_exp(dist * q))
+                else
+                    fx = (i[pos] - q) / (1.0 - f_exp(-dist * (i[pos] - q))) + q / (1.0 - f_exp(dist * q))
+                end
+                otm1 = 0.999 * otm1 + fx - itm1
+                round_to_zero(otm1)
+                itm1 = fx
+                o[pos] = otm1
+            end       
+        end
+        -- end
+
+        ::nextchannel::
+    end
+end
 
 --     <callback event="run"><![CDATA[
 -- unsigned long pos;
@@ -38,14 +135,6 @@ local otm1 = 0.0
 
 -- const float q = q_p - 0.999f;
 -- const float dist = dist_p * 40.0f + 0.1f;
-
--- q_p is a control between 0 and 1 - distortion level
-local q_p = 0.5
-local q = q_p - 0.999;
-
--- dist_p is a control between 0 and 1 - hardness level
-local dist_p = 0.5
-local dist = dist_p * 40.0 + 0.1;
 
 -- if (q == 0.0f) {
 -- 	for (pos = 0; pos < sample_count; pos++) {
@@ -75,38 +164,7 @@ local dist = dist_p * 40.0 + 0.1;
 -- 	}
 -- }
 
--- Fast exponentiation function, y = e^x
-function f_exp(x)
-    return math.exp(f)
-end
 
-if q == 0 then
-    for pos = 0,sample_count
-    do
-        if input[pos] == q then
-            fx = 1.0 / dist
-        else
-            fx = input[pos] / (1.0 - f_exp(-dist * input[pos]))
-        end
-        otm1 = 0.999 * otm1 + fx - itm1
-        round_to_zero(otm1)
-        itm1 = fx
-        output[pos] = otm1
-    end
-else
-    for pos = 0,sample_count
-    do
-        if input[pos] == q then
-		    fx = 1.0 / dist + q / (1.0 - f_exp(dist * q))
-		else
-			fx = (input[pos] - q) / (1.0 - f_exp(-dist * (input[pos] - q))) + q / (1.0 - f_exp(dist * q))
-        end
-		otm1 = 0.999 * otm1 + fx - itm1
-		round_to_zero(otm1)
-		itm1 = fx
-        output[pos] = otm1
-    end       
-end
 
 -- plugin_data->itm1 = itm1;
 -- plugin_data->otm1 = otm1;
